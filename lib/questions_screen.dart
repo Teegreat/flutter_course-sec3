2d3b0b5a945c212d9673aba12fa5df079ef6ef81
TeeGreat <toyinatofarati64@gmail.com>
2024-04-18 11:32:46 +0100

    mutating values in memory

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -34,7 +34,7 @@ class _QuestionsScreen extends State<QuestionsScreen> {
             const SizedBox(
               height: 30,
             ),
-            ...currentQuestion.answers.map(
+            ...currentQuestion.getShuffledAnswers().map(
               (answer) {
                 return AnswerButton(
                   answerText: answer,
